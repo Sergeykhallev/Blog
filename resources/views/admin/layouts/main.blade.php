3aff4cb35bf4a5923092f48d166f8b027ce3b69e
SergeyKhaleev <sergey.khaleev@gmail.com>
2022-11-30 11:46:34 +0300

    add tag for post

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href={{ asset('plugins/fontawesome-free/css/all.min.css') }}>
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href={{ asset('plugins/select2/css/select2.min.css') }}>
     <!-- Tempusdominus Bootstrap 4 -->
     <link rel="stylesheet" href={{ asset('plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}>
     <!-- iCheck -->
@@ -71,6 +72,7 @@
 <script src={{ asset('plugins/jquery-ui/jquery-ui.min.js') }}></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script src={{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}></script>
+<script src={{ asset('plugins/select2/js/select2.full.min.js') }}></script>
 
 <script src={{ asset('plugins/summernote/summernote-bs4.min.js') }}></script>
 
@@ -119,6 +121,7 @@ $.widget.bridge('uibutton', $.ui.button)
     $(function () {
         bsCustomFileInput.init();
     });
+    $('.select2').select2()
 </script>
 <style>
     .custom-file-input:lang(en)~.custom-file-label::after {
