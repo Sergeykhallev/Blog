146bb11ee800797b6c0c83e17aa2865463b7e25b
SergeyKhallev <sergey.khaleev@gmail.com>
2022-12-05 13:53:37 +0300

    add service

--- app/Http/Controllers/Admin/Post/PostController.php
+++ app/Http/Controllers/Admin/Post/PostController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Category;
 use App\Models\Post;
 
-class PostController extends Controller
+class PostController extends BaseController
 {
     public function __invoke()
     {
